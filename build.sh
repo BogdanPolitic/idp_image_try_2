4f361aef70e92da3ee6b4f110b33d11a8a27aab2
BogdanPolitic <48634138+BogdanPolitic@users.noreply.github.com>
2020-04-17 09:39:34 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 git pull
 docker build -t testapp_img .
-docker tag testapp_img bogdanpolitic/idp:try_14_04
-docker push bogdanpolitic/idp:try_14_04
+docker tag testapp_img bogdanpolitic/idp:try_17_04
+docker push bogdanpolitic/idp:try_17_04
 docker stack rm testapp
 docker stack deploy -c docker-compose-visualize.yml testapp
